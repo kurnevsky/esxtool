8d15dfe43b54f5e5c6df31ee2e4bbb1b706dca98
Evgeny Kurnevsky <kurnevsky@gmail.com>
2019-01-05 15:56:28 +0300

    Fix flags ordering.

--- src/esx/record/record_flags.rs
+++ src/esx/record/record_flags.rs
@@ -3,8 +3,8 @@ use crate::binary::Binary;
 bitflags! {
   pub struct RecordFlags: u32 {
     const Unknown = 0x0000_0020;
-    const Blocked = 0x0000_2000;
     const Persistent = 0x0000_0400;
+    const Blocked = 0x0000_2000;
   }
 }
 
